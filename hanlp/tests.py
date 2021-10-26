4d32c78dcb430c3ccb16fd0e09c340044d9f4929
anxu <axu.2009@gmail.com>
2021-10-26 09:22:10 +0800

    添加注释

--- hanlp/tests.py
+++ hanlp/tests.py
@@ -4,11 +4,16 @@ import time
 from hanlp.components.mtl.multi_task_learning import MultiTaskLearning
 from hanlp.components.mtl.tasks.tok.tag_tok import TaggingTokenization
 
+# 会在当前用户的home目录创建 .hanlp 目录并将内容下载和解压其中，如：~/.hanlp
+# > ll ~/.hanlp
+# total 0
+# drwxr-xr-x  3 axu  staff  96 10 21 17:19 mtl
+# drwxr-xr-x  3 axu  staff  96 10 21 17:20 thirdparty
+# drwxr-xr-x  3 axu  staff  96 10 21 17:20 transformers
 HanLP: MultiTaskLearning = hanlp.load(hanlp.pretrained.mtl.CLOSE_TOK_POS_NER_SRL_DEP_SDP_CON_ELECTRA_SMALL_ZH)
 tok: TaggingTokenization = HanLP['tok/fine']
 
-# tok.dict_force = None
-tok.dict_combine = {'量力度德'}
+tok.dict_combine = {'量力度德'} # 自定义此词库
 
 start = time.time()
 sentences = [
